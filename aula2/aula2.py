403b2b3cdbbf1c99174486833e2a8b0183d745e5
isaque <105675542+Isaque012@users.noreply.github.com>
2024-12-05 11:26:33 -0300

    aula 2

---
+++ aula2/aula2.py
@@ -0,0 +1,11 @@
+print('ola piru')
+nome_usuario = 'Yoichi Isaque' #tipo -> str = string 
+idade_usuario = 18 #tipo -> int =integer
+altura_usuario = 1.70  #tipo -> float = flutuante, numero de ponto flutuante(tipo float) nao quer dizer q o ponto esteja sempre no local certo.
+maior_idade = True # aoa utlizar true ou false tem q utilizar letras maiusculas no True e no False, tip -> bool= boolean.
+
+print('Bien Vienido', nome_usuario)
+print('quantos anos vc tem',nome_usuario, '?', idade_usuario)
+print('quanto de altura você possui',nome_usuario, '?', altura_usuario)
+print('Você é maior de idade por possuir 18 anos', maior_idade)
+#para fazer a interligação das variaves eu coloquei o print('...', adicionei uma virgula, e chamei a variavel e depois em seguiida criei mais uma mensagem ('?'coloquei dentro dela uma interrogação para chamar outra variavel e conmseguir completar a lacuna e a pergunta chamando ja o nome de usuario junto com o resto do enunciado.)
